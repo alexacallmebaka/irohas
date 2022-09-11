db22983fa4e4942a4d9c1e27e9db62b1d2a32c0f
james <50460460+alexacallmebaka@users.noreply.github.com>
2022-09-11 16:00:36 -0500

    comments

--- admin.py
+++ admin.py
@@ -10,17 +10,24 @@ class Admin(commands.Cog):
         self.log = log
         self.roles = roles
 
+    #Command for self-assginable roles {{{1
     @slash_command(name='grant', description='Self-assign roles.')
     async def play(self, inter: Interaction, role: str) -> None:
             
         if role in self.roles:
 
             try:
+                
+                #Grab a role object based on the role's ID.
                 to_assign = inter.user.guild.get_role(self.roles[role])
+
+                #Assign the role.
                 await inter.user.add_roles(to_assign)
+                
                 await inter.response.send_message(f'Assigned *{role}*.', ephemeral=True)
+            
             except Exception as e:
                 await inter.response.send_message(f'An error has occured, please try again.', ephemeral=True)
                 self.log.error(str(e))
         else:
-            await inter.response.send_message(f'Role *{role}* not assignable.', ephemeral=True)
+            await inter.response.send_message(f'Role *{role}* not assignable.', ephemeral=True) #1}}}
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from admin import Admin
 
 def main() -> None:
     
+    #Set up logging.
     log = logging.getLogger('irohas')
     
     stderr = logging.StreamHandler()
@@ -13,11 +14,13 @@ def main() -> None:
 
     log.addHandler(stderr)
 
+    #Create bot.
     bot = commands.Bot()
-    
+   
+    #Add commands.
     with open('creds/roles.json') as rolefile:
         bot.add_cog(Admin(bot, log, json.load(rolefile)))
-    
+   
     #Read the bot token in from external JSON.
     with open('creds/creds.json') as credfile:
         creds = json.load(credfile)
